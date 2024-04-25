eff83d2ba4c97f1983df36df753077c7acb8ea0f
Gautam0416 <gautamgandhi002@gmail.com>
2024-04-25 14:23:53 +0530

    Rent Workspace Functionality (Front-end)

--- src/app/models/rentalForm.ts
+++ src/app/models/rentalForm.ts
@@ -1,9 +1,10 @@
 export interface RentalForm {
-    id?: number;
-    location: string;
-    rentDate: string;
-    returnDate: string;
-    selectedAssets: string[];
-    selectedService: string;
-    email: string;
-  }
+  id?: number;
+  service: string;
+  location: string;
+  rentDate: Date;
+  returnDate: Date;
+  assets: string;
+  status: string;
+  email: string | null;
+}
--- src/app/pages/rent-workspace/rent-workspace.component.ts
+++ src/app/pages/rent-workspace/rent-workspace.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RentalService } from './../../services/rentalService';
+import { RentalForm } from '../../models/rentalForm';
 
 @Component({
   selector: 'app-rent-workspace',
@@ -13,7 +14,7 @@ export class RentWorkspaceComponent {
   locations: string[] = ["Location 1", "Location 2", "Location 3", "Location 4"];
   assets: string[] = ["Asset 1", "Asset 2", "Asset 3", "Asset 4"];
   today: string;
-  selectedService: string = '';
+  service: string = '';
 
   constructor(private route: ActivatedRoute, private router: Router, private rentalService: RentalService) {
     this.today = new Date().toISOString().split('T')[0];
@@ -21,15 +22,23 @@ export class RentWorkspaceComponent {
       const navigation = this.router.getCurrentNavigation();
       console.log(navigation);
       if (navigation && navigation.extras.state) {
-        this.selectedService = navigation.extras.state['select-service'];
-        console.log('Selected Service:', this.selectedService);
+        this.service = navigation.extras.state['select-service'];
+        console.log('Selected Service:', this.service);
       }
     })
   }
 
   submitForm() {
-    this.formData.selectedService = this.selectedService;
-    this.rentalService.submitRentalForm(this.formData).subscribe(
+    this.formData.service = this.service;
+    console.log(this.formData);
+
+    const rentData: RentalForm = {
+      id: 0, service: this.formData.service, location: this.formData.location, rentDate: this.formData.rentDate, returnDate: this.formData.returnDate, assets: this.formData.asset,
+      status: 'Pending', email: localStorage.getItem('email')
+    };
+
+    console.log(rentData);
+    this.rentalService.submitRentalForm(rentData).subscribe(
       response => {
         console.log('Form submitted successfully:', response);
       },
